2549347bebdcf4538265050cb6e21188c2609854
Joe Cancelliere <cancellierejm@gmail.com>
2021-09-04 11:08:01 +0100

    add DataFrame display and 'set markdowns' button

--- markdown_predictions/frontend.py
+++ markdown_predictions/frontend.py
@@ -5,3 +5,11 @@ import pandas as pd
 from markdown_predictions.trainer import get_data
 
 file_csv = st.file_uploader("Upload a PNG image", type=([".csv"]))
+
+if file_csv:
+    # TODO: Replace with get_data function from markdown_predictions.trainer
+    df = pd.read_csv(file_csv)
+    df
+
+if st.button('Set Markdowns'):
+    pass
