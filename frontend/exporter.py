10b410fa63ba0050de3385afb1bede3f91cb93d2
Joe Cancelliere <cancellierejm@gmail.com>
2021-09-07 20:00:22 +0100

    fix display issue in csv uploader and add session state df to exporter

--- frontend/exporter.py
+++ frontend/exporter.py
@@ -3,7 +3,7 @@ import numpy as np
 import pandas as pd
 
 def page_3():
-    d={'col1':['1','2','3'],'col2':[.3,.4,.1],'col3':[250,300,145]}
-    export_df = pd.DataFrame(data=d)
+    export_df = st.session_state.df[['reference_PRE','markdown_PRE']]
     st.write(export_df)
-    st.write(st.session_state.df)
+    if st.button('Download CSV'):
+        export_df.to_csv('../markdown_export.csv')
