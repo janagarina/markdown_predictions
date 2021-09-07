8f8ff501616f0803c0842c48c92d0005b7164376
Joe Cancelliere <cancellierejm@gmail.com>
2021-09-07 20:18:04 +0100

    Add sales goal summary and csv save function

--- frontend/csv_uploader.py
+++ frontend/csv_uploader.py
@@ -8,22 +8,24 @@ def page_1():
     file_csv = st.file_uploader("Upload your CSV here", type=([".csv"]))
 
     if file_csv:
-        df = get_test_data(f'../raw_data/{file_csv.name}')
+        df = get_test_data(f"../raw_data/{file_csv.name}")
         if "df" not in st.session_state:
             st.session_state.df = df
-            st.session_state.df['markdown_PRE'] = 0.0
+            st.session_state.df["markdown_PRE"] = 0.0
     st.write(st.session_state.df)
-    st.write('CSV Summary')
+    st.write("CSV Summary")
     num_products = len(st.session_state.df)
-    st.write(f'Number of products: {num_products}')
-    st.session_state.df['full_stock'] = st.session_state.df['avail_warehouse_stock_PRE'] + st.session_state.df['total_store_stock_PRE']
-    stock = st.session_state.df['full_stock'].sum()
-    st.write(f'Total stock: {stock}')
-    st.write('Stock per target & category')
+    st.write(f"Number of products: {num_products}")
+    st.session_state.df["full_stock"] = st.session_state.df["avail_warehouse_stock_PRE"] + st.session_state.df["total_store_stock_PRE"]
+    stock = st.session_state.df["full_stock"].sum()
+    st.write(f"Total stock: {stock}")
+    st.write("Stock per target & category")
     # # TODO: Updte column names after get_data merge
-    # target_stock = df.groupby(['Sous-cible','Catégorie'])['full_stock'].agg('sum')
+    # target_stock = df.groupby(["Sous-cible","Catégorie"])["full_stock"].agg("sum")
     # target_stock
-    target = st.number_input('Set unit sales target', min_value=0, max_value = 100, value=0)
+    target = st.number_input("Set unit sales target (%)", min_value=0, max_value = 100, value=0)
     target_percent = target/100
+    if "target_percent" not in st.session_state:
+        st.session_state.target_percent = target_percent
     unit_target = float(stock) * target_percent
-    st.write(f'Unit sales target: {round(unit_target)}')
+    st.write(f"Unit sales target: {round(unit_target)}")
--- frontend/exporter.py
+++ frontend/exporter.py
@@ -3,7 +3,16 @@ import numpy as np
 import pandas as pd
 
 def page_3():
-    export_df = st.session_state.df[['reference_PRE','markdown_PRE']]
+    export_df = st.session_state.df[["reference_PRE","markdown_PRE"]]
+    # TODO: update with output after model/markdown selector page updates
+    export_df["predicted_sales"] = [700,100,200,320,600,239,543,10,578]
+    total_sales_pred = export_df["predicted_sales"].sum()
     st.write(export_df)
-    if st.button('Download CSV'):
-        export_df.to_csv('../markdown_export.csv')
+    target_sales = (st.session_state.target_percent * st.session_state.df["full_stock"]).sum()
+    st.write(f"Sales target: {round(target_sales)}")
+    st.write(f"Predicted sales {total_sales_pred}")
+    pred_over_target = (total_sales_pred/round(target_sales))
+    percent_to_target = round((pred_over_target * 100),2)
+    st.write(f"Percentage achieved: {percent_to_target}%")
+    if st.button("Download CSV"):
+        export_df.to_csv("../markdown_export.csv")
