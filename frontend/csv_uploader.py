8f3fb908f0e63a05878c83ac6a14c78b08cfbd56
Joe Cancelliere <cancellierejm@gmail.com>
2021-09-14 20:10:39 +0100

    Sidebar tweaks and update to CSV uploader

--- frontend/csv_uploader.py
+++ frontend/csv_uploader.py
@@ -32,32 +32,9 @@ def page_1():
             st.session_state.df["predicted_sales"] = st.session_state.df["predicted_sales"].astype(int)
         st.write(st.session_state.df)
 
-        st.markdown("""
-        # CSV Summary
-        """)
-
-        # Calculate and display number of products in CSV
-        # num_products = len(st.session_state.df)
-        # st.write(f"Number of products: {'{:,}'.format(round(num_products))}")
-
         # Calculate and display total stock of products
         st.session_state.df["full_stock"] = st.session_state.df["avail_warehouse_stock_PRE"] + st.session_state.df["total_store_stock_PRE"]
         st.session_state.df["full_stock"] = st.session_state.df["full_stock"].astype(int)
-        # stock = st.session_state.df["full_stock"].sum()
-        # st.write(f"Total stock: {'{:,}'.format(round(stock))}")
-
-        # Group total stock by sub target and category
-        st.write("Stock per target & category")
-        target_stock = pd.DataFrame(st.session_state.df.groupby(["sub_target_PRE","product_category_PRE"])["full_stock", "predicted_sales"].agg("sum"))
-        target_stock["full_stock"] = target_stock["full_stock"].apply(lambda x: "{:,}".format(x).replace(".0", ""))
-        target_stock["predicted_sales"] = target_stock["predicted_sales"].apply(lambda x: "{:,}".format(round(x)).replace(".0", ""))
-        st.write(target_stock.rename(columns={"full_stock": "Total Stock", "predicted_sales": "Predicted Sales (Units)"}))
 
         # Allow user to set percentage unit sales goal (session state) and display number of products needed to meet goal
         target = st.number_input("Set unit sales target (%)", min_value=0, max_value = 100, step=10, value=0, key='target')
-
-        # target_percent = st.session_state.target/100
-        # unit_target = float(stock) * target_percent
-        # st.write(f"Unit sales target: {'{:,}'.format(round(unit_target))}")
-        
-        
