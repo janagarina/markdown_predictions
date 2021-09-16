abf11177c384d35dc77b2e8511eef55b206a8423
Joe Cancelliere <cancellierejm@gmail.com>
2021-09-16 22:19:14 +0100

    Implement save_plots function

--- frontend/csv_uploader.py
+++ frontend/csv_uploader.py
@@ -5,6 +5,7 @@ import numpy as np
 import pandas as pd
 from PIL import Image
 import requests
+import altair as alt
 
 from markdown_predictions.trainer import get_test_data
 
@@ -13,6 +14,38 @@ MODEL_PATH = 'markdown_model.joblib'
 COLUMNS_TO_DROP = ["season_PRE"]
 
 
+def save_plots(df):
+    df_keys = df.reference_PRE
+    for key_ref in df_keys:
+        st.session_state.product_price = df[df.reference_PRE == key_ref]["price_PRE"].iloc[0]
+        for md in range(0, 6):
+            md /= 10
+            df_tmp = df[df.reference_PRE == key_ref].copy()
+            df_tmp.markdown_PRE = md
+            columns_to_drop = COLUMNS_TO_DROP
+            if "image_url" in df_tmp.columns:
+                columns_to_drop = COLUMNS_TO_DROP + ["image_url"]
+            pred = st.session_state.model.predict(df_tmp.drop(columns_to_drop, axis=1))
+            if md == 0:
+                plot = [(round(pred[0],0), md * 10)]
+            else:
+                plot.append((round(pred[0],0), md * 100))
+        results = pd.DataFrame(plot, columns=["sales", "markdown"])
+        results["original_price"] = st.session_state.product_price
+        results["discounted_price"] = (results.original_price * ((100. - results.markdown)/100.)).apply(lambda n: round(n,2))
+        base_chart = alt.Chart(results,
+                 title=f"{df.reference_name_PRE} Unit Sales Forecast"
+                ).properties(width=700, height=400).mark_line(point=True, color="#ec3361").encode(
+            alt.X("markdown:O", title='Markdown %',  sort=None),
+            alt.Y("sales:Q", title="Predicted 2-Week Unit Sales"),
+            alt.Text("sales:Q"),
+            tooltip = [alt.Tooltip("sum(sales)", title="Predicted Unit Sales"),
+                   alt.Tooltip("sum(discounted_price)", title="Discount Price"),
+                   alt.Tooltip("sum(original_price)", title="Original Price")]
+        )
+        base_chart_text = base_chart.mark_text(dy=10, dx=5, color="#ec3361").encode(text="sales:Q")
+        final_chart = alt.layer(base_chart, base_chart_text)
+        final_chart.save(f"{key_ref}.html")
 
 def page_1():
 
@@ -42,6 +75,7 @@ def page_1():
                 columns_to_drop = COLUMNS_TO_DROP + ["image_url"]
             st.session_state.df["predicted_sales"] = st.session_state.model.predict(st.session_state.df.drop(columns_to_drop, axis=1))
             st.session_state.df["predicted_sales"] = st.session_state.df["predicted_sales"].astype(int)
+            save_plots(st.session_state.df.drop("predicted_sales",axis=1))
         st.write(st.session_state.df)
 
         # Calculate and display total stock of products
